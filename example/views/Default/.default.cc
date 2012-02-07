00b8cdbe23490708346766f71de5454b948a6f41
Jeremy Lecerf <redpist@redpist.com>
2012-02-07 22:09:33 +0100

    sort example files

---
+++ example/views/Default/.default.cc
@@ -0,0 +1,26 @@
+#include "controllers/default.hh"
+
+void View::Print()
+{
+std::cout << "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" << "\n";
+std::cout << "<html xmlns=\"http://www.w3.org/1999/xhtml\">" << "\n";
+std::cout << "	<head>" << "\n";
+std::cout << "		<title>Leeaarn</title>" << "\n";
+std::cout << "" << "\n";
+std::cout << "		<!-- METAS -->" << "\n";
+std::cout << "		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />" << "\n";
+std::cout << "		<meta name=\"author\" content=\"\" />" << "\n";
+std::cout << "		<meta name=\"description\" content=\"\" lang=\"fr\" />" << "\n";
+std::cout << "		<meta name=\"keywords\" content=\"\" lang=\"fr\" />" << "\n";
+std::cout << "	</head>" << "\n";
+std::cout << "	<body>" << "\n";
+std::cout << "        " << "\n";
+
+          std::cout << "TEST" << std::endl;
+          std::cout << str << std::endl;
+        std::cout << "" << "\n";
+std::cout << "	</body>" << "\n";
+std::cout << "</html>" << "\n";
+std::cout << "" << "\n";
+std::cout << std::endl;
+}
