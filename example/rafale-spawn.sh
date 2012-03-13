ed301647707e40ec4969fd4058f58f4cf3f689be
godric <godric@0x3f.fr>
2012-03-13 02:08:33 +0100

    godric: cleanup and bugfixes

--- example/rafale-spawn.sh
+++ example/rafale-spawn.sh
@@ -2,8 +2,8 @@
 
 ADDRESS=127.0.0.1
 PORT=9042
-USER=www-data
-GROUP=www-data
+#USER=www-data
+#GROUP=www-data
 CGI_FILE="./rafale.fcgi"
 PID_FILE="$(dirname $CGI_FILE)/$(basename $CGI_FILE).pid"
 
@@ -19,18 +19,21 @@ usage() {
 start() {
   echo "Sarting spawn-fcgi"
   if [ -f $PID_FILE ]; then
-    echo "PID file exists, if YOU ARE SURE cgi is not running remove [$PID_FILE]"; exit
+    echo "PID file exists, if YOU ARE SURE cgi is not running remove [$PID_FILE]";
+  else
+ #   spawn-fcgi -a $ADDRESS -p $PORT -u $USER -g $GROUP -P "$PID_FILE" -f "$CGI_FILE";
+   spawn-fcgi -p $PORT -P "$PID_FILE" -f "$CGI_FILE";
   fi
-  echo spawn-fcgi -a $ADDRESS -p $PORT -u $USER -g $GROUP -P "$PID_FILE" -f "$CGI_FILE";
 }
 
 stop() {
   echo "Stopping spawn-fcgi"
   if [ ! -f "$PID_FILE" ]; then
-    echo "PID file not found, if YOU ARE SURE cgi is running, try force-stop"; exit
+    echo "PID file not found, if YOU ARE SURE cgi is running, try force-stop"; 
+  else
+    kill $(cat "$PID_FILE")
+    rm "$PID_FILE" 
   fi
-  kill $(cat "$PID_FILE")
-  rm "$PID_FILE" 
 }
 
 force_stop() {
