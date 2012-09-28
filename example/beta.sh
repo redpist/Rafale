8a93ee174b8a2692cf1b3854294ea3a0d41e067f
Jeremy Lecerf <redpist@redpist.com>
2012-09-28 19:25:41 +0200

    example

--- example/beta.sh
+++ example/beta.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cmake ..
-make
+env RAFALE_BUILD_TYPE=BETA cmake ..
+env RAFALE_BUILD_TYPE=BETA make
 kill -TERM `pgrep -f "./example-beta"`
 sleep 1
-spawn-fcgi -a 127.0.0.1 -p 9042 -u www-data -g www-data -f ./example-beta
+spawn-fcgi -a 127.0.0.1 -p 9041 -u www-data -g www-data -f ./example-beta
--- example/release.sh
+++ example/release.sh
@@ -2,6 +2,6 @@
 
 env RAFALE_BUILD_TYPE=RELEASE cmake ..
 env RAFALE_BUILD_TYPE=RELEASE make
-kill `pgrep -f "./example"`
+kill -TERM `pgrep -f "./example"`
 sleep 1
 spawn-fcgi -a 127.0.0.1 -p 9041 -u www-data -g www-data -f ./example
